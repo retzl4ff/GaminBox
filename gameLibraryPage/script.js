1e653147b90e53a91871674088e334c693efd09e
João Victor Della Giustina Gonçalvez <jvictordgg@gmail.com>
2022-11-08 07:45:44 -0300

    Main Branch
    Mudei o menu

--- gameLibraryPage/script.js
+++ gameLibraryPage/script.js
@@ -8,4 +8,8 @@ gameImage.addEventListener("pointerout", function retorna() {
     gameImage.setAttribute('src', 'assets/mariogame-png.png')
 })
 
+let url1 = "../login.html"
 
+function login(){
+    location = url1
+}
