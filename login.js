83c5c2f5d1e969e8a1a3d1e433c12019093ea602
João Alexandre Retzlaff de Matos <joaoaretzlaff@gmail.com>
2022-11-09 12:28:47 -0300

    Origin
    definitive

---
+++ login.js
@@ -0,0 +1,9 @@
+let url = "gamepage.html"
+let email = document.querySelector('input#email')
+let senha = document.querySelector('input#senha')
+let button = document.getElementById('entrar')
+let aviso = document.querySelector('p')
+
+function volta() {
+    location = url
+}
